f3f6026724b125b71b7f057819ac656ff5302a0d
techlordd <techlordd@gmail.com>
2023-02-23 08:51:27 +0100

    components and routes added

---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+
+export default class CreateExercise extends Component{
+    render(){
+        return (
+            <div>You are on Create Exercise component</div>
+        )
+    }
+}
